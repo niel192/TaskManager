0172df18f8ca154a2141a7059fa215ef03dbdd7a
NIEL <nielsheladia@gmail.com>
2023-05-29 17:20:27 -0700

    init push

---
+++ src/main/java/com/example/taskManager/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.taskManager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    String Hello(){
+        return "Hello";
+    }
+
+
+}
